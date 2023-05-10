8339df71823ea2ca7f6807dcfde37af25c50cc63
sibmaks <sibmaks@vk.com>
2023-05-10 19:23:25 +0700

    Update dependencies

--- src/main/java/com/github/sib_energy_craft/metallurgy/iron_craft_table/load/Items.java
+++ src/main/java/com/github/sib_energy_craft/metallurgy/iron_craft_table/load/Items.java
@@ -4,11 +4,13 @@ import com.github.sib_energy_craft.energy_api.utils.Identifiers;
 import com.github.sib_energy_craft.metallurgy.iron_craft_table.item.IronHammerItem;
 import com.github.sib_energy_craft.metallurgy.iron_craft_table.item.MetalShearsItem;
 import com.github.sib_energy_craft.sec_utils.load.ModRegistrar;
-import com.github.sib_energy_craft.sec_utils.utils.ItemUtils;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.util.Rarity;
+
+import static com.github.sib_energy_craft.sec_utils.utils.ItemUtils.register;
+import static com.github.sib_energy_craft.sec_utils.utils.ItemUtils.registerBlockItem;
 
 /**
  * @since 0.0.1
@@ -20,24 +22,20 @@ public final class Items implements ModRegistrar {
     public static final MetalShearsItem METAL_SHEARS;
 
     static {
-        var tableSettings = new Item.Settings();
-
-        var ironCraftingTable = new BlockItem(Blocks.IRON_CRAFTING_TABLE.entity(), tableSettings);
-        IRON_CRAFTING_TABLE = ItemUtils.register(ItemGroups.FUNCTIONAL, Identifiers.of("iron_crafting_table"),
-                ironCraftingTable);
+        IRON_CRAFTING_TABLE = registerBlockItem(ItemGroups.FUNCTIONAL,Blocks.IRON_CRAFTING_TABLE);
 
         var hammerSettings = new Item.Settings()
                 .maxCount(1)
                 .rarity(Rarity.COMMON)
                 .maxDamage(80);
         var ironHammerItem = new IronHammerItem(hammerSettings);
-        IRON_HAMMER = ItemUtils.register(ItemGroups.TOOLS, Identifiers.of("iron_hammer"), ironHammerItem);
+        IRON_HAMMER = register(ItemGroups.TOOLS, Identifiers.of("iron_hammer"), ironHammerItem);
 
         var shearsSettings = new Item.Settings()
                 .maxCount(1)
                 .rarity(Rarity.COMMON)
                 .maxDamage(60);
         var metalShearsItem = new MetalShearsItem(shearsSettings);
-        METAL_SHEARS = ItemUtils.register(ItemGroups.TOOLS, Identifiers.of("metal_shears"), metalShearsItem);
+        METAL_SHEARS = register(ItemGroups.TOOLS, Identifiers.of("metal_shears"), metalShearsItem);
     }
 }
